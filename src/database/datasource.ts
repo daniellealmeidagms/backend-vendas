97fd2750f7ff03421db59ea99fc23d928ff48cb9
JohnathanLT <johnathanlt@hotmail.com>
2022-11-22 15:48:38 -0300

    remoção

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,13 +1,6 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import Acao from './entities/Acao';
-import Aluno from './entities/Aluno';
-import Cargo from './entities/Cargo';
-import Curso from './entities/Curso';
-import Horario from './entities/Horario';
-import Membro from './entities/Membro';
-import Necessidade from './entities/Necessidade';
-import Responsavel from './entities/Responsavel';
+
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -15,18 +8,11 @@ export const AppDataSource = new DataSource({
   port: 5432,
   username: 'postgres',
   password: 'postgres',
-  database: 'api-gestao-teste',
+  database: 'api-vendas',
   synchronize: true,
   logging: true,
   entities: [
-    Acao,
-    Aluno,
-    Cargo,
-    Curso,
-    Horario,
-    Membro,
-    Necessidade,
-    Responsavel,
+    
   ],
   subscribers: [],
   migrations: [],
