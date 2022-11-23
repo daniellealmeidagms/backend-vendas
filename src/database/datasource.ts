e72e04edde08f61f9ae3808458d5dda5829ae603
Felipe Olanda Damaceno <115737094+FelipeOlanda@users.noreply.github.com>
2022-11-23 15:37:47 -0300

    Deu Erro

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,5 +1,12 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import Aquisicoes from './entities/Aquisicao';
+import Cliente from './entities/Cliente';
+import Endereco from './entities/Endereco';
+import { Fornecedor } from './entities/Fornecedor';
+import Lojista from './entities/Lojista';
+import Preco from './entities/Preco';
+import Produto from './entities/Produto';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -11,7 +18,13 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: true,
   entities: [
-
+    Aquisicoes,
+    Cliente,
+    Endereco,
+    Fornecedor,
+    Lojista,
+    Preco,
+    Produto,
   ],
   subscribers: [],
   migrations: [],
