2796f9cce32feaec7e60cf45c255c0478e69a813
Felipe Olanda Damaceno <115737094+FelipeOlanda@users.noreply.github.com>
2022-11-23 15:40:44 -0300

    erro 2

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+<<<<<<< Updated upstream
 import Aquisicoes from './entities/Aquisicao';
 import Cliente from './entities/Cliente';
 import Endereco from './entities/Endereco';
@@ -7,6 +8,8 @@ import { Fornecedor } from './entities/Fornecedor';
 import Lojista from './entities/Lojista';
 import Preco from './entities/Preco';
 import Produto from './entities/Produto';
+=======
+>>>>>>> Stashed changes
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -18,6 +21,7 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: true,
   entities: [
+<<<<<<< Updated upstream
     Aquisicoes,
     Cliente,
     Endereco,
@@ -25,6 +29,9 @@ export const AppDataSource = new DataSource({
     Lojista,
     Preco,
     Produto,
+=======
+
+>>>>>>> Stashed changes
   ],
   subscribers: [],
   migrations: [],
