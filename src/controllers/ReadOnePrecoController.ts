44482672effb769852f3366e60136b7de80568b1
Felipe Olanda Damaceno <felipe_olanda@yahoo.com.br>
2022-11-29 13:24:29 -0300

    Read One Erro

--- src/controllers/ReadOnePrecoController.ts
+++ src/controllers/ReadOnePrecoController.ts
@@ -6,8 +6,9 @@ import { ReadOnePrecoService } from 'src/Services/ReadOnePrecoService';
 export class ReadOnePrecoController {
     async handle(request: Request, response: Response) {
         const { id } = request.params;
+        const idInt = parseInt(id);
         const service = new ReadOnePrecoService();
-        const preco = await service.execute();
+        const preco = await service.execute({ idInt })
         return response.json(preco);
     }
 }
