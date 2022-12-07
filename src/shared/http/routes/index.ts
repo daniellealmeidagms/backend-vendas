e71a81de4516ee71adedd0d58baa30848e2907c8
Danielle Almeida <daniellealmeidagms@gmail.com>
2022-12-07 20:45:31 -0300

    atualiza index rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
-import precoRoutes from './preco';
-import produtoRoutes from './produto';
+import enderecoRoutes from './entities/endereco';
+import fornecedorRoutes from './entities/fornecedor';
+import lojistaRoutes from './entities/lojista';
+import precoRoutes from './entities/preco';
+import produtoRoutes from './entities/produto';
+import vendaRoutes from './entities/venda';
 
 const routes = Router();
 
+routes.use('/enderecos', enderecoRoutes)
+routes.use('/fornecedores', fornecedorRoutes);
+routes.use('/lojistas', lojistaRoutes);
 routes.use('/precos', precoRoutes);
 routes.use('/produtos', produtoRoutes);
+routes.use('/vendas', vendaRoutes)
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Home Page' });
