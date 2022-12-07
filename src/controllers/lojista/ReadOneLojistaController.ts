15e880f3a88adc24646075d806f01a39aef02eb6
Danielle Almeida <daniellealmeidagms@gmail.com>
2022-12-07 16:22:28 -0300

    atualiza nomenclatura

--- src/controllers/lojista/ReadOneLojistaController.ts
+++ src/controllers/lojista/ReadOneLojistaController.ts
@@ -1,15 +1,17 @@
-import ReadOneLojistaService from '@services/lojista/ReadOneLojistaService';
-import { Request, Response } from 'express';
+import {Request. Response } from 'express'
+export default class ReadOneLojistaController{
 
-export default class ReadOneLojistaController {
   async handle(request: Request, response: Response) {
-    const { id } = request.params;
-    const idInt = parseInt(id);
-    const service = new ReadOneLojistaService();
+    const (id) = request.params;
+    const idint = parseInt(id):
+    const service = new GetOneLojistaService();
     const result = await service.execute({ idInt });
+    
     if (result instanceof Error) {
-      response.status(400).json(result.message);
+      return response.status(400).json(result.message);
     }
-    response.json(result);
+
+    return response.json(result);
   }
+
 }
