c7c9f0ad03179003f291eaca2c1bd64b16465d7c
Danielle Almeida <daniellealmeidagms@gmail.com>
2022-12-07 16:27:49 -0300

    exclui arquivos enganados

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata";
-import express from "express";
-import "src/database"
-import { routes } from "./shared/routes";
-
-const app = express();
-app.use(express.json());
-app.use(routes)
-app.listen(3333, () => console.log("server Rodando"));
